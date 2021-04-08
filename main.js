6fd1c167fc4c7b6a457fb9c88f3a7f3931de4c89
Kat White <kat.white.a@gmail.com>
2021-04-08 11:43:34 -0600

    Add data.js file to contain meal arrays

---
+++ main.js
@@ -0,0 +1,23 @@
+// variables
+var letsCookButton = document.querySelector(".lets-cook-button")
+
+// event listeners
+letsCookButton.addEventListener("click", displayChosenDish)
+
+//event handlers
+
+function displayChosenDish () {
+  // take input from chosed radio button
+  // change inner text of "".your-meal-here" to
+  // value of random array below
+ array[Math.floor(Math.random() * array.length)];
+
+}
+
+
+//user selects dish option
+//user clicks let's cook
+//event lister on lets cook button
+//take unput from radio button
+//access the array related to each radio button
+//randomly display dish on 'display-meal-box'
