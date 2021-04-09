0114578616196b4262da541c1ae92c7f635d04aa
Kat White <kat.white.a@gmail.com>
2021-04-08 20:54:45 -0600

    Write codeblock to hide/reveal

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 
 // variables
 var letsCookButton = document.querySelector(".lets-cook-button")
+var youShouldMakeText = document.querySelector(".you-should-make")
+var cookpot = document.querySelector("img")
+var dishText = document.querySelector(".dish-text")
+)
 
 // event listeners
-letsCookButton.addEventListener("click", displayChosenDish)
+letsCookButton.addEventListener("click", displayChosenDish, reveal)
 
 //event handlers
 
@@ -18,16 +22,36 @@ function getRadioValue() {
   return value
 }
 
-function displayChosenDish () {
-  var chosenDish = getRadioValue()
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
 
-  // change inner text of "".your-meal-here" to
-  // value of random array below
- //array[Math.floor(Math.random() * array.length)];
+function reveal() {
+  cookpot.classList.add("hidden")
+  youShouldMakeText.classList.remove("hidden")
+}
 
+function displayChosenDish() {
+  var chosenCourse = getRadioValue()
+  var courseList
+  if(chosenCourse === "sides") {
+    courseList = sides
+  }else if (chosenCourse === "mains") {
+    courseList = mains
+  }else {
+    courseList = desserts
+  }
+  var randomIndex = getRandomIndex(courseList)
+  var displayedDish = courseList[randomIndex]
+  dishText.innerText.add(displayedDish)
+};
 
-//user selects dish option
-//user clicks let's cook
-//access the array related to each radio button
-//randomly display dish on 'display-meal-box'
+//hide - cookpot
+// reveal you should Make
+//toggle!
+
+  // change inner text of "".your-meal-here" to displayedDish
+  //hide "your-meal-here"
+  //reveal after button clicked
+  // reveal smaller fot "You should make":
+  //larger font displayedDish
